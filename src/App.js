ce48cf39f476eab8b594f737cc46350c6d0414a6
sadiqabbasi <61027168+sadiqabbasi@users.noreply.github.com>
2022-05-24 10:04:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Text from './component/Text';
+import Input from './component/Input';
+function App() {
+  return (
+    <div className="App">
+      <Text/>
+      <Input/>
+    </div>
+  );
+}
+
+export default App;
